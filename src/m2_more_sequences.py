49a050c5459b096249977009de9d9a71d68d04a2
hickshj <hickshj@rose-hulman.edu>
2018-10-15 10:40:12 -0400

    Who's Yehoodi

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -18,7 +18,7 @@ def main():
     """ Calls the   TEST   functions in this module. """
     run_test_shortest_string()
     run_test_index_of_largest_number()
-    # run_test_number_of_stutters()
+    run_test_number_of_stutters()
     # run_test_is_palindrome()
     # run_test_count_same()
 
@@ -250,9 +250,14 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(s) - 1):
+        if s[k] == s[k + 1]:
+            count = count + 1
+    return count
 
 
 def run_test_is_palindrome():
