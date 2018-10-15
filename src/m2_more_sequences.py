a0f08db37c4761110164ec9ff3cbd79c0c78851e
hickshj <hickshj@rose-hulman.edu>
2018-10-15 10:58:01 -0400

    Who's Yehoodi

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -20,7 +20,7 @@ def main():
     run_test_index_of_largest_number()
     run_test_number_of_stutters()
     run_test_is_palindrome()
-    # run_test_count_same()
+    run_test_count_same()
 
 
 # ----------------------------------------------------------------------
@@ -398,9 +398,14 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # Done: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            count = count + 1
+    return count
 
 
 # ----------------------------------------------------------------------
